e208d3c3be076e199c63b6c786d4e01fa891dea3
lpg2709 <leonardo_guarezi@hotmail.com>
2025-03-07 08:05:22 -0300

    FIX check zig version on build script

--- src/main.zig
+++ src/main.zig
@@ -1,16 +1,9 @@
 const std = @import("std");
 const net = std.net;
 const builtin = @import("builtin");
-const zig_version = builtin.zig_version;
-const is_zig_12 = zig_version.minor == 12;
 const Server = @import("server.zig").Server;
 
 pub fn main() !void {
-    const stderr = std.io.getStdErr();
-    if (!is_zig_12) {
-        try stderr.writer().print("Invalid zig version. Expected: zig 0.12.0 | Get: {}.{}.{}\n", .{ zig_version.major, zig_version.minor, zig_version.patch });
-        return;
-    }
     var server = try Server.init();
     defer server.deinit();
 
